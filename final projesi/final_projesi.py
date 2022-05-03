fad38401e4efc203fa746d387e700d88cf6f352d
Mehmet Fatih Taskin <makinist8118@gmail.com>
2022-05-03 22:02:47 +0300

    ekran gövde oranlarının ayarlanması

--- final projesi/final_projesi.py
+++ final projesi/final_projesi.py
@@ -6,10 +6,10 @@ canvas = Canvas(master , height=450 , width=750)
 canvas.pack()
 
 frame_ust = Frame(master , bg="#eab676")
-frame_ust.place(relx=0.1 , rely=0.1 , relheight=0.1 , relwidth=0.75)
+frame_ust.place(relx=0.1 , rely=0.1 , relheight=0.1 , relwidth=0.74)
 
 frame_altsol = Frame(master , bg="#eab676")
-frame_altsol.place(relx=0.1 , rely=0.21 , relheight=0.23 , relwidth=0.5)
+frame_altsol.place(relx=0.1 , rely=0.21 , relheight=0.56 , relwidth=0.23)
 
 frame_altsağ = Frame(master , bg="#eab676")
 frame_altsağ.place(relx=0.34 , rely=0.21 , relheight=0.56 , relwidth=0.5)
