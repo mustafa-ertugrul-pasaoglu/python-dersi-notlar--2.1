07e69a9be42644947b3717510985d941dad7d233
Mehmet Fatih Taskin <makinist8118@gmail.com>
2022-05-03 22:31:10 +0300

    giriş yazısı değişiklikleri

--- final projesi/final_projesi.py
+++ final projesi/final_projesi.py
@@ -1,18 +1,21 @@
 
+from cProfile import label
 from tkinter import *
+from turtle import left
 master = Tk()
 
 canvas = Canvas(master , height=450 , width=750)
 canvas.pack()
 
 frame_ust = Frame(master , bg="#eab676")
-frame_ust.place(relx=0.1 , rely=0.1 , relheight=0.1 , relwidth=0.74)
+frame_ust.place(relx=0.1 , rely=0.1 , relheight=0.1 , relwidth=0.75)
 
-frame_altsol = Frame(master , bg="#eab676")
-frame_altsol.place(relx=0.1 , rely=0.21 , relheight=0.56 , relwidth=0.23)
+frame_alt = Frame(master , bg="#eab676")
+frame_alt.place(relx=0.1 , rely=0.21 , relheight=0.56 , relwidth=0.75)
 
-frame_altsağ = Frame(master , bg="#eab676")
-frame_altsağ.place(relx=0.34 , rely=0.21 , relheight=0.56 , relwidth=0.5)
+giris_etiketi = Label(frame_ust, bg="#eab676" ,text="Giriş yapmak için şifrenizi giriniz" , font="Times New Roman")
+giris_etiketi.pack(padx=10 , pady=10 , side=left)
+
 
 
 
