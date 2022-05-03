e291a270b92c41910d4da7a0a1f7e51d894f5fe5
Mehmet Fatih Taskin <makinist8118@gmail.com>
2022-05-03 22:34:57 +0300

    bazı denemeler ve işlemler

--- final projesi/final_projesi.py
+++ final projesi/final_projesi.py
@@ -13,8 +13,8 @@ frame_ust.place(relx=0.1 , rely=0.1 , relheight=0.1 , relwidth=0.75)
 frame_alt = Frame(master , bg="#eab676")
 frame_alt.place(relx=0.1 , rely=0.21 , relheight=0.56 , relwidth=0.75)
 
-giris_etiketi = Label(frame_ust, bg="#eab676" ,text="Giriş yapmak için şifrenizi giriniz" , font="Times New Roman")
-giris_etiketi.pack(padx=10 , pady=10 , side=left)
+giris_etiketi = Label(frame_ust, bg ="#eab676", text ="Giriş yapmak için şifrenizi giriniz", font="Verdana 12 bold")
+giris_etiketi.pack(padx=10 , pady=10 , side=LEFT)
 
 
 
