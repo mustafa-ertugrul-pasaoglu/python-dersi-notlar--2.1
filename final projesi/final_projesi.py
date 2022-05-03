524aa95723be8cd01a347429219aff81c8f22e85
Mehmet Fatih Taskin <makinist8118@gmail.com>
2022-05-03 21:23:05 +0300

    üst bölümün renklerinin ayarlanması ve köşelerden oran verilmesi

--- final projesi/final_projesi.py
+++ final projesi/final_projesi.py
@@ -5,5 +5,9 @@ master = Tk()
 canvas = Canvas(master , height=450 , width=750)
 canvas.pack()
 
+frame_ust = Frame(master , bg="#eab676")
+frame_ust.place(relx=0.1 , rely=0.1 , relheight=0.1 , relwidth=0.75)
+
+
 master.mainloop()
 
